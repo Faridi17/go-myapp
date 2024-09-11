38c4d1db2bb85d1cbe11ea3c3fa0f980aa1b2b06
Faridi17 <azkafaridi@gmail.com>
2024-09-11 22:21:13 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Faridi17/go-myapp
+
+go 1.23.0
+
+require github.com/Faridi17/go-mymodules/v2 v2.0.0-20240911151413-d6c5cc0037a2
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_say_hello "github.com/Faridi17/go-mymodules/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.MathDivide(1,0))
+}
